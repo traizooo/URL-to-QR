a826a3e621d0911b267bbec2eccf2a173c806fd4
Michal <michal.owsiejew23@gmail.com>
2023-06-29 13:17:38 +0200

    Commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/convert', (req, res) => {
   qrCode.pipe(res);
 });
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
