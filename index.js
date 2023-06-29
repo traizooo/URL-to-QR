06a4ba7ee6d71b8baa7d76ad634f8b6e8247b249
Michal <michal.owsiejew23@gmail.com>
2023-06-29 13:08:09 +0200

    Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const qr = require('qr-image');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Front-end/index.html');
+});
+
+app.post('/convert', (req, res) => {
+  const url = req.body.url;
+  const qrCode = qr.image(url, { type: 'png' });
+  res.type('png');
+  qrCode.pipe(res);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
